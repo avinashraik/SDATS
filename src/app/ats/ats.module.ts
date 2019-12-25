4320370766baa4c7119390f4deaaea1268ecd4f7
Saksham Srivastava <Saksham Srivastava@DESKTOP-ETMF01D>
2019-12-25 10:24:10 +0530

    added master pages

--- src/app/ats/ats.module.ts
+++ src/app/ats/ats.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { AtsRoutingModule } from './ats-routing.module';
 import { AddCandidateComponent } from './components/add-candidate/add-candidate.component';
-
+import {MatCardModule, MatButtonModule} from '@angular/material'
 
 
 @NgModule({
   declarations: [AddCandidateComponent],
   imports: [
     CommonModule,
-    AtsRoutingModule
-    
+    AtsRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class AtsModule { }
